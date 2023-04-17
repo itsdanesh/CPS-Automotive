35cb23755a5b7493ee91091b7ae1810adcc7bf57
@alttum <alttum@chalmers.se>
2023-04-17 20:05:25 +0200

    Include cpp files for coverage

--- src/PrimeChecker.cpp
+++ src/PrimeChecker.cpp
@@ -2,9 +2,11 @@
 
 bool PrimeChecker::isPrime(uint16_t n) {
     bool retVal{true};
+    // Check if number is less than 2
     if (n<2 || 0 == n%2) {
         retVal = false;
     }
+    // Check if number is prime
     else {
         for(uint16_t i{3}; (i*i) <= n; i += 2) {
             if (0 == n%i) {
